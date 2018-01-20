53e51fefef78a20019b2da2a674fef4d37b831f4
Kaz Yoshikawa <kaz@digitallynx.com>
2018-01-20 09:30:30 -0500

    Initial Commit

---
+++ BlendFuncTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BlendFuncTest
+//
+//  Created by Kaz Yoshikawa on 1/20/18.
+//  Copyright © 2018 Kaz Yoshikawa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
